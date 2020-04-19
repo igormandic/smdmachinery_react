9afa3689b0f153da514be77e6ea261b352f10704
mi17506 <petar.pejovic8@gmail.com>
2020-04-19 16:22:56 +0200

    Radjeno na homepage-u

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -6,7 +6,7 @@ class ProductList extends Component {
     render() {
 
         return (
-            <div className="product_middle">
+            <div className="company_middle">
                 <div className="company_text"> <p> Neki tekst koji je namenjen da stoji za vile masine.
                     I koji opisuje ukratko o cemu se tu radi.
                     Navedeno za par masina...Neki tekst koji je namenjen da stoji za vile masine.
@@ -15,44 +15,49 @@ class ProductList extends Component {
                     I koji opisuje ukratko o cemu se tu radi.
                     Navedeno za par masina...Neki tekst koji je namenjen da stoji za vile masine.
                     I koji opisuje ukratko o cemu se tu radi.
-                    Navedeno za par masina... </p> </div>
-                <div className="productList">
-                            <button className="btn_productList">
-                                <div>
-                                    <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
-                                    <p>265</p>
-                                    <img src="/WILLE/265/masina1.png" alt="product" className="slika_pl"/>
-                                </div>
-                            </button>
-                            <button className="btn_productList">
-                                <div>
-                                    <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
-                                    <p>265</p>
-                                    <img src="/WILLE/265/masina1.png" alt="product" className="slika_pl"/>
-                                </div>
-                            </button>
-                            <button className="btn_productList">
-                                <div>
-                                    <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
-                                    <p>265</p>
-                                    <img src="/WILLE/265/masina1.png" alt="product" className="slika_pl"/>
-                                </div>
-                            </button>
-                            <button className="btn_productList">
-                                <div>
-                                    <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
-                                    <p>265</p>
-                                    <img src="/WILLE/265/masina1.png" alt="product" className="slika_pl"/>
-                                </div>
-                            </button>
-                            <button className="btn_productList">
-                                <div>
-                                    <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
-                                    <p>265</p>
-                                    <img src="/WILLE/265/masina1.png" alt="product" className="slika_pl"/>
-                                </div>
-                            </button>
+                    Navedeno za par masina... </p> 
                 </div>
+
+                <button className="btn_productList">
+                    <div>
+                        <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
+                        <img src="/WILLE/265/masina3.jpg" alt="product" className="slika_pl"/>
+                        <p>WILLE 265</p>
+                        <button className="saznaj_vise_pl" > Saznajte vise </button> 
+                    </div>
+                </button>
+                <button className="btn_productList">
+                    <div>
+                        <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
+                        <img src="/WILLE/265/masina3.jpg" alt="product" className="slika_pl"/>
+                        <p>WILLE 265</p>
+                        <button className="saznaj_vise_pl" > Saznajte vise </button> 
+                    </div>
+                </button>
+                <button className="btn_productList">
+                    <div>
+                        <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
+                        <img src="/WILLE/265/masina3.jpg" alt="product" className="slika_pl"/>
+                        <p>WILLE 265</p>
+                        <button className="saznaj_vise_pl" > Saznajte vise </button> 
+                    </div>
+                </button>
+                <button className="btn_productList">
+                    <div>
+                        <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
+                        <img src="/WILLE/265/masina3.jpg" alt="product" className="slika_pl"/>
+                        <p>WILLE 265</p>
+                        <button className="saznaj_vise_pl" > Saznajte vise </button> 
+                    </div>
+                </button>
+                <button className="btn_productList">
+                    <div>
+                        <img className="machine_logo_pl" src="/img/Wille/wille-logo.png" alt="machine_logo"/>
+                        <img src="/WILLE/265/masina3.jpg" alt="product" className="slika_pl"/>
+                        <p>WILLE 265</p>
+                        <button className="saznaj_vise_pl" > Saznajte vise </button> 
+                    </div>
+                </button>
             </div>
     );
     }
