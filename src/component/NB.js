6ea8b302ddb872108b207125148be402ba1c3531
igormandic <igormandic996@gmail.com>
2020-04-28 22:20:15 +0200

    Dodato zatamljeno dugme

--- src/component/NB.js
+++ src/component/NB.js
@@ -12,18 +12,20 @@ class NB extends Component {
     let images = [];
     let linkForAllProducts = "";
     let linkForAllAttachment = "";
+    let ind = true;
+    let currentPaht = window.location.pathname;
     if (this.props.name === "WILLE") {
       products = ["265", "375", "465", "665", "865"];
       images = ["/WILLE/265/masina1.png","/WILLE/375/375_pose_thumb.png",
                 "/WILLE/465/465_pose_thumb.png","/WILLE/665/665iv_pose_thumb_v2.png",
-                "/WILLE/865/865iv_pose_thumb.png"];
+                "/WILLE/865/865iv_pose_thumb.png"]; 
       linkForAllProducts = `/company/${this.props.name}/product`;
     } else if (this.props.name === "Broddson") {
       products = ["Nordic", "Scandia", "Fighter"];
       images = ["/Broddson/Nordic/masina3.png","/Broddson/Scandia/masina3.png", "/Broddson/Fighter/masina3.png"];
+      ind = false;
     }
-
-    linkForAllAttachment = `/company/${this.props.name}/attachments`
+    linkForAllAttachment = `/company/${this.props.name}/attachments`;
     let pathToProducts = []
     for (let i = 0; i<products.length; i++) {
       let pathToProduct = `/company/${this.props.name}/product/${products[i]}`;
@@ -41,14 +43,14 @@ class NB extends Component {
         <img src="/fa_bars.png" className="img_bars" alt="fa_bars"></img>
         </label>
 
-          <ul>
+          <ul >
             <li><Link to="/">{__('Home page')}</Link></li>
             <li className="firme"><Link to={linkForAllProducts}>Proizvodi</Link>
               <ul>
                 {pathToProducts}
               </ul>
             </li>
-              <li className="li_posle_padajuceg"><a href={linkForAllAttachment} className="nsk">Nastavci</a></li>
+              {ind?<li className="li_posle_padajuceg"><Link to={linkForAllAttachment} className="nsk">Nastavci</Link></li>:<li className="li_posle_padajuceg"><Link to={currentPaht} className="nsk" style={{color:"black"}}>Nastavci</Link></li>}
           </ul>
         </nav>
         </div>
