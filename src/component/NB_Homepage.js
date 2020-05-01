aaf836823adcf2f122b10ebbbb1abd09dcdbabc4
igormandic <igormandic996@gmail.com>
2020-05-01 22:12:03 +0200

    Sredjen nb

--- src/component/NB_Homepage.js
+++ src/component/NB_Homepage.js
@@ -5,18 +5,36 @@ import { Link } from 'react-router-dom';
 import { __ } from '../utils/i18n';
 
 class NB_Homepage extends Component {
-render(){
+  constructor(init){
+    super(init);
+    this.handleChange = this.handleChange.bind(this);
+  }
+ 
+  handleChange({target}){
+    let nb = document.getElementById('NB_response');
+    if (target.checked){
+      nb.style.display = "block";
+    } else {
+      nb.style.display = "none";
+    }
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      let currentScrollPos = window.pageYOffset;
+      
+      if (prevScrollpos <= currentScrollPos - 20) {
+        document.getElementById("NB_response").style.display = "none";
+        let cb = document.getElementById('check');
+        cb.checked = '';
+      } 
+      prevScrollpos = currentScrollPos;
+    }
+  }
+  render(){
 
   return (
     <div>
     <div className="NB_css">
       <nav>
-
-        <input type="checkbox" id="check"/>
-        <label htmlFor="check" className="checkbtn">
-        <img src="/fa_bars.png" className="img_bars" alt="fa_bars"></img>
-        </label>
-
         <ul>
           <li><a href="/">{__('Home page')}</a></li>
           <li><a href="#companyList">{__('Companies')}</a>
@@ -38,6 +56,31 @@ render(){
           </div>
         </ul>
       </nav>
+      </div>
+      <div className="tri_crte">
+        <label htmlFor="check" className="checkbtn">
+          <input type="checkbox" id="check" onClick={this.handleChange}
+                        defaultChecked={this.props.complete}/>
+            <img src="/fa_bars.png" className="img_bars" alt="fa_bars"></img>
+        </label>
+      </div>
+      <div className="NB_response" id="NB_response">
+      <ul>
+            <li><Link to="/"> {__('Home page')} </Link></li>
+            <li><a href="#companyList"> {__('Companies')} </a>
+              <ul>
+                <li><Link to="/company/WILLE">Wille</Link></li>
+                <li><Link to="/company/Broddson">Broddson</Link></li>
+                <li><a href="https://www.boschung.com/">Boschung </a></li>
+                <li><Link to="/company/WILLE">Dorotea mekaniska </Link></li>
+              </ul>
+            </li>
+            <div className="li_posle_padajuceg">
+            <li><Link to="/aboutUs">{__('About us')}</Link></li>
+            <li><a href="/#" className="nsk">{__('Servis')}</a></li>
+            <li><a href="/contact" className="nsk">{__('Contact')}</a></li>
+          </div>
+          </ul>
       </div>
     </div>
   );
