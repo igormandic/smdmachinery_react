6755f269e95d0f65d4f02807f7426ca7be449587
mi17506 <petar.pejovic8@gmail.com>
2020-05-01 21:54:00 +0200

    Gotovooo

--- src/component/NB_Homepage.js
+++ src/component/NB_Homepage.js
@@ -26,7 +26,7 @@ render(){
               <li><Link to="/company/Broddson"> 
                 <img className="machine_logo" src="/Firme/broddson/logo.png" alt="machine_logo"/> Broddson</Link></li>
               <li><a href="https://www.boschung.com/">
-                <img className="machine_logo" src="/Firme/boschung/logo.jpg" alt="machine_logo"/> Boschung </a></li>
+                <img className="machine_logo" src="/Firme/boschung/logo2.png" alt="machine_logo"/> Boschung </a></li>
               <li><Link to="/company/WILLE">
                 <img className="machine_logo" src="/Firme/dorotea mekaniska/logo.png" alt="machine_logo"/> Dorotea mekaniska </Link></li>
             </ul>
